5a538b746511fb06254f029960684f0379c160bb
Rody Huancas <rodyhuancas.04@gmail.com>
2024-01-11 22:42:38 -0500

    Add: headlessui

--- src/pages/admin/Profile.jsx
+++ src/pages/admin/Profile.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+// icons
 import { RiEdit2Line, RiShieldCheckLine } from "react-icons/ri"
+// headlessui
+import { Switch } from '@headlessui/react'
 
 export const Profile = () => {
+  const [enabled, setEnabled] = useState(false)
+
   return (
     <>
       {/* Profile */}
@@ -25,11 +32,11 @@ export const Profile = () => {
           </div>
 
           {/* Inputs */}
-          <div className="flex items-center mb-6">
+          <div className="flex md:items-center mb-6">
             <div className="w-1/4">
               <p>Full Name <span className="text-red-500">*</span></p>
             </div>
-            <div className="flex-1 flex items-center gap-4">
+            <div className="flex-1 flex md:items-center flex-col md:flex-row gap-4">
               <div className="w-full" >
                 <input type="text" className="w-full py-2 px-4 outline-none rounded-lg bg-secondary-900" placeholder="Name" />
               </div>
@@ -98,8 +105,8 @@ export const Profile = () => {
         </div>
       </div>
 
-      {/* change password */}
-      <div className="bg-secondary-100 p-8 rounded-xl">
+      {/* Change password */}
+      <div className="bg-secondary-100 p-8 rounded-xl mb-8">
         <h1 className="text-xl text-gray-100">User and Password</h1>
         <hr className="my-8 border-gray-500/30" />
         <form className="mb-6">
@@ -133,6 +140,55 @@ export const Profile = () => {
             <button className="bg-green-600/70 hover:bg-green-600 transition-colors duration-300 py-2 px-4 rounded-lg text-gray-100">Activate</button>
           </div>
         </div>
+      </div>
+
+      {/* Connected accounts */}
+      <div className="bg-secondary-100 p-8 rounded-xl mb-8">
+        <h1 className="text-xl text-gray-100">Connected Accounts</h1>
+        <hr className="my-8 border-gray-500/30" />
+
+        <div className="flex items-center gap-5 bg-green-600/10 p-4 rounded-lg border border-dashed border-green-600 mb-8">
+          <div className="flex justify-center">
+            <RiShieldCheckLine className="text-5xl text-green-500" />
+          </div>
+          <div className="flex-1">
+            <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus dolorum repellendus numquam. <Link to="/" className="text-green-400">Read more...</Link></p>
+          </div>
+        </div>
+
+        <form className="mb-6">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-4">
+              <img src="https://cdn-icons-png.flaticon.com/512/300/300221.png" alt="google" className="w-8 h-8 object-cover" />
+              <div className="flex flex-col gap-y-1">
+                <h5 className="text-gray-100">Google</h5>
+                <p className="text-gray-500 text-sm">Lorem ipsum dolor sit.</p>
+              </div>
+            </div>
+            <Switch
+              checked={enabled}
+              onChange={setEnabled}
+              className={`${enabled ? 'bg-primary' : 'bg-secondary-900'
+                } relative inline-flex h-6 w-11 items-center rounded-full`}
+            >
+              <span className="sr-only">Enable notifications</span>
+              <span
+                className={`${enabled ? 'translate-x-6 bg-secondary-900' : 'translate-x-1 bg-gray-500'
+                  } inline-block h-4 w-4 transform rounded-full transition`}
+              />
+            </Switch>
+          </div>
+
+          <hr className="my-8 border-gray-500/30 border-dashed" />
+
+          <div className="flex items-center justify-between">
+            <div>
+              <h5 className="text-gray-100 text-xl mb-1">Password</h5>
+              <p className="text-sm text-gray-500">******************</p>
+            </div>
+            <button className="bg-secondary-900/50 py-2 px-4 rounded-lg hover:bg-secondary-900 hover:text-gray-100 transition-colors duration-300">Change Password</button>
+          </div>
+        </form>
       </div>
     </>
   )
