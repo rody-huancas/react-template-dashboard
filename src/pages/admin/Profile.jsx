7a65964bd430cdb556c5d29b172762b24d3908cc
Rody Huancas <rodyhuancas.04@gmail.com>
2024-01-12 22:20:52 -0500

    Add: section activate account

--- src/pages/admin/Profile.jsx
+++ src/pages/admin/Profile.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
 // icons
-import { RiEdit2Line, RiShieldCheckLine } from "react-icons/ri"
+import { RiEdit2Line, RiErrorWarningLine, RiShieldCheckLine } from "react-icons/ri"
 // headlessui
 import { Switch } from '@headlessui/react'
 
@@ -100,7 +100,7 @@ export const Profile = () => {
 
         <div className="flex justify-end">
           <button className="bg-primary/80 text-black py-2 px-4 rounded-lg hover:bg-primary transition-colors font-medium">
-            Guardar
+            Save
           </button>
         </div>
       </div>
@@ -227,6 +227,74 @@ export const Profile = () => {
             </Switch>
           </div>
         </form>
+      </div>
+
+      {/* Email Preferences */}
+      <div className="bg-secondary-100 p-8 rounded-xl mb-8">
+        <h1 className="text-xl text-gray-100">E-mail Notifications</h1>
+        <hr className="my-8 border-gray-500/30" />
+
+        <form className="mb-6">
+          <div className="flex items-center gap-4">
+            <input type="checkbox" id="id1" className="accent-primary" />
+            <div className="flex flex-col gap-y-1">
+              <label htmlFor="id1" className="text-gray-100">Successful Payments</label>
+              <p className="text-gray-500 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+          <hr className="my-8 border-gray-500/30" />
+
+          <div className="flex items-center gap-4">
+            <input type="checkbox" id="id2" className="accent-primary" />
+            <div className="flex flex-col gap-y-1">
+              <label htmlFor="id2" className="text-gray-100">Successful Payments</label>
+              <p className="text-gray-500 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+          <hr className="my-8 border-gray-500/30" />
+
+          <div className="flex items-center gap-4">
+            <input type="checkbox" id="id3" className="accent-primary" />
+            <div className="flex flex-col gap-y-1">
+              <label htmlFor="id3" className="text-gray-100">Successful Payments</label>
+              <p className="text-gray-500 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+          <hr className="my-8 border-gray-500/30" />
+        <div className="flex justify-end">
+          <button className="bg-primary/80 text-black py-2 px-4 rounded-lg hover:bg-primary transition-colors font-medium">
+            Save
+          </button>
+        </div>
+        </form>
+      </div>
+
+      {/* Ianctive accounts */}
+      <div className="bg-secondary-100 p-8 rounded-xl mb-8">
+        <h1 className="text-xl text-gray-100">Inactive Accounts</h1>
+        <hr className="my-8 border-gray-500/30" />
+
+        <div className="flex items-center gap-5 bg-yellow-600/10 p-4 rounded-lg border border-dashed border-yellow-600 mb-8">
+          <div className="flex justify-center">
+            <RiErrorWarningLine className="text-5xl text-yellow-500" />
+          </div>
+          <div className="flex-1">
+            <h5 className="text-gray-100 mb-2">You Are Deactivating Your Account</h5>
+            <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus dolorum repellendus numquam, nesciunt officia cum! <Link className="text-blue-600">Learn more...</Link></p>
+          </div>
+        </div>
+        <form className="flex items-center gap-4">
+          <input type="checkbox" className="accent-primary" id="idInactive" />
+          <label htmlFor="idInactive" className="text-gray-500">I confirm my account deactivation</label>
+        </form>
+
+        <hr className="my-8 border-gray-500/30" />
+
+        <div className="flex justify-end">
+          <button className="bg-red-500/80 text-gray-100 py-2 px-4 rounded-lg hover:bg-red-500 transition-colors font-medium">
+            Desactivate Account
+          </button>
+        </div>
       </div>
     </>
   )
