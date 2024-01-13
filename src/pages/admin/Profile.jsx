e7f6006d5e35c16d466a55952211c6183eff5e91
Rody Huancas <rodyhuancas.04@gmail.com>
2024-01-12 19:31:27 -0500

    Add: Connected Accounts

--- src/pages/admin/Profile.jsx
+++ src/pages/admin/Profile.jsx
@@ -179,14 +179,52 @@ export const Profile = () => {
             </Switch>
           </div>
 
-          <hr className="my-8 border-gray-500/30 border-dashed" />
+          <hr className="my-8 border-gray-500/30" />
 
           <div className="flex items-center justify-between">
-            <div>
-              <h5 className="text-gray-100 text-xl mb-1">Password</h5>
-              <p className="text-sm text-gray-500">******************</p>
+            <div className="flex items-center gap-4">
+              <img src="https://cdn.icon-icons.com/icons2/1488/PNG/512/5347-github_102542.png" alt="github" className="w-8 h-8 object-cover" />
+              <div className="flex flex-col gap-y-1">
+                <h5 className="text-gray-100">GitHub</h5>
+                <p className="text-gray-500 text-sm">Lorem ipsum dolor sit.</p>
+              </div>
             </div>
-            <button className="bg-secondary-900/50 py-2 px-4 rounded-lg hover:bg-secondary-900 hover:text-gray-100 transition-colors duration-300">Change Password</button>
+            <Switch
+              checked={enabled}
+              onChange={setEnabled}
+              className={`${enabled ? 'bg-primary' : 'bg-secondary-900'
+                } relative inline-flex h-6 w-11 items-center rounded-full`}
+            >
+              <span className="sr-only">Enable notifications</span>
+              <span
+                className={`${enabled ? 'translate-x-6 bg-secondary-900' : 'translate-x-1 bg-gray-500'
+                  } inline-block h-4 w-4 transform rounded-full transition`}
+              />
+            </Switch>
+          </div>
+
+          <hr className="my-8 border-gray-500/30" />
+
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-4">
+              <img src="https://icones.pro/wp-content/uploads/2022/08/logo-slack.png" alt="slack" className="w-8 h-8 object-cover" />
+              <div className="flex flex-col gap-y-1">
+                <h5 className="text-gray-100">Slack</h5>
+                <p className="text-gray-500 text-sm">Lorem ipsum dolor sit.</p>
+              </div>
+            </div>
+            <Switch
+              checked={enabled}
+              onChange={setEnabled}
+              className={`${enabled ? 'bg-primary' : 'bg-secondary-900'
+                } relative inline-flex h-6 w-11 items-center rounded-full`}
+            >
+              <span className="sr-only">Enable notifications</span>
+              <span
+                className={`${enabled ? 'translate-x-6 bg-secondary-900' : 'translate-x-1 bg-gray-500'
+                  } inline-block h-4 w-4 transform rounded-full transition`}
+              />
+            </Switch>
           </div>
         </form>
       </div>
