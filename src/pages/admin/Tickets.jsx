239089522865960f3a928c6fe1483b72e5bd4290
Rody Huancas <rodyhuancas.04@gmail.com>
2024-01-15 11:53:39 -0500

    Add: section ticket

--- src/pages/admin/Tickets.jsx
+++ src/pages/admin/Tickets.jsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
 import {
   RiAddFill,
+  RiArrowLeftSLine,
   RiArrowRightLine,
   RiArrowRightSLine,
+  RiDiscussLine,
+  RiFileTextLine,
   RiFilter2Fill,
+  RiGithubLine,
   RiSearch2Line,
+  RiTicketLine,
+  RiTwitterLine,
 } from "react-icons/ri";
 import { Disclosure, Tab, Transition } from "@headlessui/react";
 
@@ -85,6 +91,7 @@ export const Tickets = () => {
         </div>
 
         <Tab.Panels className="mt-8">
+          {/* overview */}
           <Tab.Panel>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
               <div className="bg-secondary-100 p-8 rounded-lg">
@@ -211,7 +218,7 @@ export const Tickets = () => {
               <div className="bg-secondary-100 p-8 rounded-lg">
                 {/* title card */}
                 <div className="flex items-center justify-between mb-8">
-                    <h1 className="text-white text-2xl">FAQ</h1>
+                  <h1 className="text-white text-2xl">FAQ</h1>
                   <Link className="flex items-center gap-2 text-primary">
                     All FAQ <RiArrowRightLine />
                   </Link>
@@ -330,7 +337,222 @@ export const Tickets = () => {
               </div>
             </div>
           </Tab.Panel>
-          <Tab.Panel>Content 2</Tab.Panel>
+
+          {/* tickets */}
+          <Tab.Panel>
+            <div className="bg-secondary-100 p-8 rounded-lg grid grid-cols-1 md:grid-cols-4 gap-8">
+              {/* section 1 */}
+              <div className="col-span-3 p-8">
+                <form>
+                  <div className="relative">
+                    <RiSearch2Line className="absolute top-1/2 -translate-y-1/2 left-4" />
+                    <input
+                      type="text"
+                      className="bg-secondary-900 outline-none py-3 pr-4 pl-10 rounded-lg placeholder:text-gray-500 w-full"
+                      placeholder="Search for anything"
+                    />
+                  </div>
+                </form>
+                <h2 className="text-white text-2xl py-8">Public Tickets</h2>
+                {/* Tickets */}
+                <div>
+                  <div className="flex flex-col gap-2 mb-8">
+                    {/* ticket */}
+                    <div className="flex items-center gap-4">
+                      <RiTicketLine className="text-2xl text-yellow-500" />
+                      <Link
+                        to=""
+                        className="text-xl hover:text-yellow-500 transition-colors duration-300"
+                      >
+                        How to use Netronic whit Django Framework?
+                      </Link>
+                      <span className="bg-secondary-900 text-white text-sm py-0.5 px-2 rounded-lg">
+                        React
+                      </span>
+                    </div>
+                    <div className="px-10">
+                      <p className="text-gray-500">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Ad ipsa nihil illo harum aut nisi recusandae, magni
+                        eveniet. Perspiciatis beatae eum ullam. Odit tempora
+                        minima, odio qui magnam debitis. Voluptatum.
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="flex flex-col gap-2 mb-8">
+                    {/* ticket */}
+                    <div className="flex items-center gap-4">
+                      <RiTicketLine className="text-2xl text-blue-500" />
+                      <Link
+                        to=""
+                        className="text-xl hover:text-blue-500 transition-colors duration-300"
+                      >
+                        How to use Netronic whit Django Framework?
+                      </Link>
+                      <span className="bg-secondary-900 text-white text-sm py-0.5 px-2 rounded-lg">
+                        React
+                      </span>
+                    </div>
+                    <div className="px-10">
+                      <p className="text-gray-500">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Ad ipsa nihil illo harum aut nisi recusandae, magni
+                        eveniet. Perspiciatis beatae eum ullam. Odit tempora
+                        minima, odio qui magnam debitis. Voluptatum.
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="flex flex-col gap-2 mb-8">
+                    {/* ticket */}
+                    <div className="flex items-center gap-4">
+                      <RiTicketLine className="text-2xl text-green-500" />
+                      <Link
+                        to=""
+                        className="text-xl hover:text-green-500 transition-colors duration-300"
+                      >
+                        How to use Netronic whit Django Framework?
+                      </Link>
+                      <span className="bg-secondary-900 text-white text-sm py-0.5 px-2 rounded-lg">
+                        React
+                      </span>
+                    </div>
+                    <div className="px-10">
+                      <p className="text-gray-500">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Ad ipsa nihil illo harum aut nisi recusandae, magni
+                        eveniet. Perspiciatis beatae eum ullam. Odit tempora
+                        minima, odio qui magnam debitis. Voluptatum.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                {/* pagination */}
+                <div className="p-8 flex justify-center">
+                  <nav className="flex items-center gap-2">
+                    <button className="p-2 hover:bg-secondary-900 rounded-lg transition-colors hover:text-gray-100 duration-300">
+                      <RiArrowLeftSLine />
+                    </button>
+                    <div className="flex items-center gap-1">
+                      <button className="py-2 px-4 hover:bg-secondary-900 rounded-lg transition-colors hover:text-gray-100 duration-300">
+                        1
+                      </button>
+                      <button className="py-2 px-4 bg-secondary-900 rounded-lg text-gray-100">
+                        2
+                      </button>
+                      <button className="py-2 px-4 hover:bg-secondary-900 rounded-lg transition-colors hover:text-gray-100 duration-300">
+                        3
+                      </button>
+                      <button className="py-2 px-4 hover:bg-secondary-900 rounded-lg transition-colors hover:text-gray-100 duration-300">
+                        4
+                      </button>
+                      <button className="py-2 px-4 hover:bg-secondary-900 rounded-lg transition-colors hover:text-gray-100 duration-300">
+                        5
+                      </button>
+                    </div>
+                    <button className="p-2 hover:bg-secondary-900 rounded-lg transition-colors hover:text-gray-100 duration-300">
+                      <RiArrowRightSLine />
+                    </button>
+                  </nav>
+                </div>
+              </div>
+
+              {/* section 2 */}
+              <div>
+                <div className="bg-secondary-900 p-8 rounded-lg mb-8">
+                  <h2 className="text-2xl text-white mb-8">More Channels</h2>
+                  <div>
+                    <div className="flex items-center gap-4 mb-8">
+                      <RiFileTextLine className="text-4xl text-primary" />
+                      <div className="flex flex-col gap-1">
+                        <h5 className="text-white">Project Briefing</h5>
+                        <p className="text-xs">
+                          Lorem ipsum dolor{" "}
+                          <Link to="" className="text-primary">
+                            Sit Amet
+                          </Link>
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className="flex items-center gap-4 mb-8">
+                      <RiDiscussLine className="text-4xl text-primary" />
+                      <div className="flex flex-col gap-1">
+                        <h5 className="text-white">More to discuss?</h5>
+                        <p className="text-xs">
+                          Lorem ipsum dolor{" "}
+                          <Link to="" className="text-primary">
+                            Sit Amet
+                          </Link>
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className="flex items-center gap-4 mb-8">
+                      <RiTwitterLine className="text-4xl text-primary" />
+                      <div className="flex flex-col gap-1">
+                        <h5 className="text-white">Latest News</h5>
+                        <p className="text-xs">
+                          Lorem ipsum dolor{" "}
+                          <Link to="" className="text-primary">
+                            Sit Amet
+                          </Link>
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className="flex items-center gap-4 mb-8">
+                      <RiGithubLine className="text-4xl text-primary" />
+                      <div className="flex flex-col gap-1">
+                        <h5 className="text-white">GitHub Access</h5>
+                        <p className="text-xs">
+                          Lorem ipsum dolor{" "}
+                          <Link to="" className="text-primary">
+                            Sit Amet
+                          </Link>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="bg-secondary-900 p-8 rounded-lg">
+                  <h2 className="text-2xl text-white mb-8">Documentation</h2>
+
+                  <ul className="flex flex-col gap-y-4">
+                    <li>
+                        <Link to="" className="flex items-center gap-2 hover:text-primary transition-colors">
+                            <RiArrowRightSLine/> Angular Admin
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="" className="flex items-center gap-2 hover:text-primary transition-colors">
+                            <RiArrowRightSLine/> React Admin
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="" className="flex items-center gap-2 hover:text-primary transition-colors">
+                            <RiArrowRightSLine/> Vue Admin
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="" className="flex items-center gap-2 hover:text-primary transition-colors">
+                            <RiArrowRightSLine/> Svelte Admin
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="" className="flex items-center gap-2 hover:text-primary transition-colors">
+                            <RiArrowRightSLine/> Astro Admin
+                        </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </Tab.Panel>
+
+          {/* FAQ */}
           <Tab.Panel>Content 3</Tab.Panel>
         </Tab.Panels>
       </Tab.Group>
